e848bc7f3a142615958dc3705d2a989537f80bf5
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-08-19 13:06:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+document.getElementById("userForm").addEventListener("submit",function (evetn) {
+	event.preventDefault();
+
+	const age=document.getElementById("age").value.trim();
+	const name=document.getElementById("name").value.trim();
+
+
+	if(age===""||name===""){
+		alert("Please enter valid details");
+		return;
+	}
+
+	const ageCheck=new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(age>18){
+				resolve(`Welcome, ${name}. You can vote.`)
+			}else{
+				reject(`Oh soory ${name}. You aren't old enough.`);
+			}
+		},4000)
+	})
+
+	ageCheck.then((msg)=>{
+		alert(msg);
+	}).catch((err)=>{
+		alert(err);
+	})
+})
